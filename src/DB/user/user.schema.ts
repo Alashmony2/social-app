2dde60b1a20a72adac1f2db01bcf9c9aa49a0bba
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-09-06 15:06:29 +0300

    adding user schema

---
+++ src/DB/user/user.schema.ts
@@ -0,0 +1,63 @@
+import { Schema } from "mongoose";
+import { IUser } from "../../utils/common/interface";
+import { GENDER, SYS_ROLE, USER_AGENT } from "../../utils/common/enum";
+
+export const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      minlength: 2,
+      maxlength: 20,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minlength: 2,
+      maxlength: 20,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: function () {
+        if (this.userAgent == USER_AGENT.google) return false;
+        return true;
+      },
+    },
+    credentialUpdatedAt: Date,
+    phoneNumber: String,
+    role: {
+      type: String,
+      enum: SYS_ROLE,
+      default: SYS_ROLE.user,
+    },
+    gender: { type: String, enum: GENDER, default: GENDER.male },
+    userAgent: {
+      type: String,
+      enum: USER_AGENT,
+      default: USER_AGENT.local,
+    },
+    otp: { type: String },
+    otpExpiry: { type: Date },
+  },
+  { timestamps: true }
+);
+
+userSchema
+  .virtual("fullName")
+  .get(function () {
+    return this.firstName + " " + this.lastName;
+  })
+  .set(function (value: string) {
+    const [fName, lName] = value.split(" ");
+    this.firstName = fName as string;
+    this.lastName = lName as string;
+  });
