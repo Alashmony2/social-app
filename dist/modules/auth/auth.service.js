2fc5f49c5fab6624297265fb5e3a6d16caa46072
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-09-06 23:21:18 +0300

    create error exceptions

--- dist/modules/auth/auth.service.js
+++ dist/modules/auth/auth.service.js
@@ -1,10 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const user_model_1 = require("../../DB/user/user.model");
+const error_1 = require("../../utils/error");
 class AuthService {
     constructor() { }
-    register(req, res, next) {
+    async register(req, res, next) {
         //get data form request
         const registerDto = req.body;
+        //check user exist 
+        const userExist = await user_model_1.User.findOne({ email: registerDto.email });
+        if (userExist) {
+            throw new error_1.ConflictException("User already exist");
+        }
     }
 }
 exports.default = new AuthService();
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,19 @@
 import type { NextFunction, Request, Response } from "express";
 import { RegisterDTO } from "./auth.dto";
+import { User } from "../../DB/user/user.model";
+import { error } from "console";
+import { ConflictException } from "../../utils/error";
 
 class AuthService {
   constructor() {}
-  register(req: Request, res: Response, next: NextFunction) {
+  async register(req: Request, res: Response, next: NextFunction) {
     //get data form request
     const registerDto:RegisterDTO = req.body;
+    //check user exist 
+    const userExist = await User.findOne({email:registerDto.email});
+    if(userExist){
+      throw new ConflictException("User already exist")
+    }
   }
 }
 export default new AuthService();
