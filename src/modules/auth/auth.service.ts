22bd8264dab5fc54fc282fb9b5999542fe4c97cb
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-09-13 22:28:49 +0300

    create validation for register

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
 import { ConfirmEmailDTO, LoginDTO, RegisterDTO } from "./auth.dto";
 import { User } from "../../DB/user/user.model";
-import { ConflictException, NotAuthorizedException, NotFoundException } from "../../utils/error";
+import {
+  BadRequestException,
+  ConflictException,
+  NotAuthorizedException,
+  NotFoundException,
+} from "../../utils/error";
 import { AbstractRepository } from "../../DB/abstract.repository";
 import { IUser } from "../../utils/common/interface";
 import { UserRepository } from "../../DB/user/user.repository";
 import { AuthFactoryService } from "./factory";
 import { compareHash } from "../../utils/hash";
 import { sendEmail } from "../../utils/email";
+import * as authValidation from "./auth.validation";
+import { log } from "console";
 
 class AuthService {
   private userRepository = new UserRepository();
@@ -16,6 +23,19 @@ class AuthService {
   register = async (req: Request, res: Response, next: NextFunction) => {
     //get data form request
     const registerDto: RegisterDTO = req.body;
+    //validation
+    const result = authValidation.registerSchema.safeParse(registerDto);
+    console.log(result);
+    if (!result.success) {
+      let errMessages = result.error.issues.map((issue) => ({
+        path: issue.path[0],
+        message: issue.message,
+      }));
+      console.log(errMessages);
+      throw new BadRequestException("Validation Failed", errMessages);
+
+    }
+
     //check user exist
     const userExist = await this.userRepository.exist({
       email: registerDto.email,
@@ -26,13 +46,13 @@ class AuthService {
     //prepare data
     const user = this.authFactoryService.register(registerDto);
     //save into DB
-    const createdUser = await this.userRepository.create(user) ;
+    const createdUser = await this.userRepository.create(user);
     //send email
     await sendEmail({
       to: createdUser.email,
       subject: "verify your email",
       html: `<p>Your otp to verify your account is ${createdUser.otp}</p>`,
-    })
+    });
     //send response
     return res.status(201).json({
       message: "User Created Successfully",
@@ -41,8 +61,8 @@ class AuthService {
     });
   };
 
-  confirmEmail = async (req: Request, res: Response, next: NextFunction)=>{
-    const confirmEmailDto : ConfirmEmailDTO = req.body;
+  confirmEmail = async (req: Request, res: Response, next: NextFunction) => {
+    const confirmEmailDto: ConfirmEmailDTO = req.body;
     const userExist = await this.userRepository.exist({
       email: confirmEmailDto.email,
     });
@@ -50,24 +70,26 @@ class AuthService {
       throw new NotFoundException("User not Found");
     }
     //check otp
-    if(userExist.otp !== confirmEmailDto.otp){
-      throw new NotAuthorizedException("Invalid OTP")
+    if (userExist.otp !== confirmEmailDto.otp) {
+      throw new NotAuthorizedException("Invalid OTP");
     }
     //update user
     userExist.otp = undefined as unknown as string;
     userExist.otpExpiryAt = undefined as unknown as Date;
     userExist.isVerified = true;
     //save into DB
-    await this.userRepository.update({email:confirmEmailDto.email},userExist,{new:true} as any)
+    await this.userRepository.update(
+      { email: confirmEmailDto.email },
+      userExist,
+      { new: true } as any
+    );
     //send response
     return res.status(200).json({
       message: "User Email Confirmed Successfully",
       success: true,
       data: userExist,
-    })
-
-  }
-
+    });
+  };
 
   login = async (req: Request, res: Response, next: NextFunction) => {
     //get data form request
@@ -80,18 +102,18 @@ class AuthService {
       throw new NotFoundException("User not Found");
     }
     //check is password valid
-    const isPasswordValid = compareHash(loginDto.password,userExist.password)
-    if(!isPasswordValid){
-        throw new NotAuthorizedException("Invalid Credentials")
+    const isPasswordValid = compareHash(loginDto.password, userExist.password);
+    if (!isPasswordValid) {
+      throw new NotAuthorizedException("Invalid Credentials");
     }
-    if(!userExist.isVerified){
-      throw new NotAuthorizedException("Verify your account first")
+    if (!userExist.isVerified) {
+      throw new NotAuthorizedException("Verify your account first");
     }
     return res.status(200).json({
       message: "User Login Successfully",
       success: true,
       data: userExist,
-    })
-  }
+    });
+  };
 }
 export default new AuthService();
