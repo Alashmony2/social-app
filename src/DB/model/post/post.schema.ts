3a1408e09225100774539c558bd3f2564b3fb106
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-09-28 22:36:14 +0300

    create post schema

---
+++ src/DB/model/post/post.schema.ts
@@ -0,0 +1,38 @@
+import { Schema } from "mongoose";
+import { IPost, REACTION } from "../../../utils";
+
+const reactionSchema = new Schema(
+  {
+    reaction: {
+      type: Number,
+      enum: REACTION,
+      default: REACTION.like,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const postSchema = new Schema<IPost>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: function () {
+        if (this.attachments.length) return false;
+        return true;
+      },
+      trim: true,
+    },
+    likes: [reactionSchema],
+  },
+  { timestamps: true }
+);
